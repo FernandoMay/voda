12136918026d8168b3a5acf890f372c993f633b9
FernandoMay <fmay130@gmail.com>
2019-07-19 18:48:45 -0500

    letters

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vodafone'),
+        backgroundColor: Colors.orange.shade900,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Arbol(),
+          Center( child: Text('Hello World!')),
+
+        ],
+      ),
+    );
+  }
+}
+
+class Arbol extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(Icons.ondemand_video),
+                Container(
+                  child: Text('One'),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.ondemand_video),
+                Container(
+                  child: Text('One'),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.ondemand_video),
+                Container(
+                  child: Text('One'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
